f56e969a179ca86f3773040c85e9796d5de3338e
Abirami Pradisha M <64889261+AbiramiPradisha@users.noreply.github.com>
2020-06-06 22:37:28 +0530

    Create IS ODD?.java

---
+++ LBH_MILE 1/DAY 4/IS ODD?.java
@@ -0,0 +1,17 @@
+public class IsOdd {
+
+	public static void main(String[] args) {
+		odd(15);
+
+	}
+	public static int odd(int input1) {
+		if(input1%2!=0){
+			return 2;
+		}
+		else{
+			return 1;
+		}
+
+	}
+
+}

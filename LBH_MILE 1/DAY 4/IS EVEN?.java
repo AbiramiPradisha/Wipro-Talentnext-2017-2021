38db67473637ccc56597de48f9bd0980b5465b0d
Abirami Pradisha M <64889261+AbiramiPradisha@users.noreply.github.com>
2020-06-06 22:23:23 +0530

    Create IS EVEN?.java

---
+++ LBH_MILE 1/DAY 4/IS EVEN?.java
@@ -0,0 +1,18 @@
+public class returnstatement {
+
+	public static void main(String[] args) {
+		even(15);
+		
+		
+		}
+	public static int even(int n){	
+		if(n%2==0) {
+			return 2;
+		}
+		else {
+			return 1;
+		}
+
+	}
+
+}
